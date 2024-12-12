c65035598d23b7462cdb7cee171ef32441cbed8e
Kharanshu Wanare <kharanshuw@gmail.com>
2024-12-12 13:23:46 +0530

    tailwind css demo

---
+++ 03-vite-react-app-tailwind-css-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Chai from './chai'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Chai></Chai>
+    </>
+  )
+}
+
+export default App
---
+++ 03-vite-react-app-tailwind-css-demo/src/chai.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import React from "react";
+
+function Chai() {
+
+
+    return (
+        <>
+            <h1 className="text-black bg-green-500">TAILWIND CSS</h1>
+        </>
+    )
+}
+
+export default Chai;
