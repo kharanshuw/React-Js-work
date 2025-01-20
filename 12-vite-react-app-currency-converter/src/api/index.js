e36340aaadb6f5a86cb6d2db4a8dc81be8a2cb8f
Kharanshu Wanare <kharanshuw@gmail.com>
2025-01-20 14:13:10 +0530

    app update

--- 12-vite-react-app-currency-converter/src/api/index.js
+++ 12-vite-react-app-currency-converter/src/api/index.js
@@ -2,7 +2,8 @@
 
 export const getCurrency = async (from,to) => {
 
-    let url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_RUWtpIFaMCTdl3NmPqtYws99FCPVTb94S8064IZk&currencies=${to}&base_currency=${from}`;
+//use your own key from freecurrencyapi
+    let url = `https://api.freecurrencyapi.com/v1/latest?apikey=${key}&currencies=${to}&base_currency=${from}`;
 
     const response = await fetch(url,
         {
@@ -20,7 +21,7 @@ export const getCurrency = async (from,to) => {
 
 
 export const getCurrencyList = async () => {
-    let url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_RUWtpIFaMCTdl3NmPqtYws99FCPVTb94S8064IZk";
+    let url = "https://api.freecurrencyapi.com/v1/latest?apikey=${key}";
 
     const response = await fetch(url,
         {
