e151c7e1df781c2f34f5ce7eda6dac099c6a6e24
Kharanshu Wanare <kharanshuw@gmail.com>
2025-01-21 15:21:02 +0530

    react router demo

---
+++ 13-react-router-dom/src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Navbar } from './component/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { Home } from './component/Home';
+import { Login } from "./component/Login";
+import { About } from './component/About';
+
+function App() {
+
+  let router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+
+      {
+        path: "/about",
+        element: <About></About>
+      }
+    ]
+  )
+
+  return (
+    <div>
+
+
+
+      <RouterProvider router={router}>
+
+        <div>
+          <Navbar>
+
+          </Navbar>
+        </div>
+
+      </RouterProvider>
+    </div>
+  );
+
+
+
+
+}
+
+export default App
---
+++ 13-react-router-dom/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router"
+
+
+export function Navbar() {
+    return (
+        <div className="flex flex-row gap-3 mt-2 mb-2 border pt-2 pb-2 justify-center " >
+
+            <Link to="/">home</Link>
+            <Link to="/about">about</Link>
+            <Link to="/login">login</Link>
+        </div>
+    )
+}
+
+
